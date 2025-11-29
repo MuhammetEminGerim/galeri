6b3f49ecb8310825a14906ae4538a7b28abaa914
Muhammet Emin <muhammetemingerim@outlook.com>
2025-11-29 17:30:49 +0300

    fix: add firebase storage domain to next.config

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
